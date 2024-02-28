ccca7e7f730151c6628579645745f0637135ca5e
Freddy Tomada <freddytomada7@gmail.com>
2024-02-28 09:54:09 -0400

    add getAllProducts action

--- app/action.ts
+++ app/action.ts
@@ -1,4 +1,11 @@
 export async function getAllProducts() {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const products = await response.json();
+
+  return products;
+}
+
+export async function getFeaturedProducts() {
   const response = await fetch("https://fakestoreapi.com/products?limit=4");
   const products = await response.json();
 
