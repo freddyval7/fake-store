03da62b40f181b3296818c26bbe4ee004dd3264b
Freddy Tomada <freddytomada7@gmail.com>
2024-02-28 10:23:33 -0400

    fix featured products card in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -18,32 +18,32 @@ export default async function Page() {
       <h1 className="text-3xl font-bold mb-8">Featured Products</h1>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
         {products.map((product) => (
-          <Card
-            className="h-[400px] flex flex-col justify-center items-center"
-            key={product.id}
-          >
-            <CardHeader className="h-3/5">
-              <Image
-                priority
-                className="object-contain h-full w-full"
-                width={500}
-                height={500}
-                src={product.image}
-                alt={product.title}
-              />
-            </CardHeader>
-            <CardContent className="flex h-auto flex-col justify-start gap-2">
-              <h2 className="text-center">{product.title}</h2>
-              <p className="text-green-700 text-center text-lg">
-                ${product.price}
-              </p>
-            </CardContent>
-            <CardFooter className="w-full">
-              <Link href={`/product/${product.id}`}>
-                <Button className="w-full">View Product</Button>
-              </Link>
-            </CardFooter>
-          </Card>
+          <div
+          className="md:max-h-[400px] p-8 flex flex-col justify-between gap-4 md:gap-0 shadow-2xl rounded-md border-2"
+          key={product.id}
+        >
+          <div className="h-3/5">
+            <Image
+              priority
+              className="object-contain h-full w-full"
+              width={500}
+              height={500}
+              src={product.image}
+              alt={product.title}
+            />
+          </div>
+          <div className="flex h-auto flex-col justify-start gap-2">
+            <h2 className="whitespace-nowrap truncate font-bold">{product.title}</h2>
+            <p className="text-green-700 text-lg">${product.price}</p>
+          </div>
+          <div className="w-full">
+            <Link className="w-full" href={`/product/${product.id}`}>
+              <Button className="w-full">
+                View Product
+              </Button>
+            </Link>
+          </div>
+        </div>
         ))}
       </div>
     </main>
