4f373a5f3d4b603edd0bbcf76930b26b2485c835
Freddy Tomada <freddytomada7@gmail.com>
2024-03-01 10:34:43 -0400

    add message when cart is empty

--- app/cart/cart.tsx
+++ app/cart/cart.tsx
@@ -86,92 +86,102 @@ export default function Cart() {
       <div className="grid md:grid-cols-2 gap-8">
         <div className="space-y-4">
           <h2 className="font-bold text-lg">Your shopping cart</h2>
-            <div className="space-y-4">
-              {cart.map((product) => (
-                <>
-                  <div key={product.id} className="flex items-center gap-4">
-                    <div className="rounded-md border-2 shadow-2xl p-4">
-                      <Image
-                        width={500}
-                        height={500}
-                        className="w-14"
-                        src={product.image}
-                        alt="product"
-                      />
-                    </div>
-                    <div className="flex flex-col justify-between w-full gap-2">
-                      <h3 className="font-bold">{product.title}</h3>
-                      <span className="text-muted-foreground">
-                        ${product.price} USD
-                      </span>
-                      <div className="flex items-center justify-between">
-                        <div className="flex items-center gap-4 text-sm">
-                          <p>Quantity: {product.quantity}</p>
-                          <div className="flex items-center gap-2">
-                            <Button
-                              disabled={product.quantity === 1}
-                              onClick={() => sustractQuantity(product.id)}
-                              className="h-8"
+          <div className="space-y-4">
+            {cart.length === 0 && (
+              <p className="text-muted-foreground my-8">
+                Your cart is empty, add some products to continue...
+              </p>
+            )}
+            {cart.map((product) => (
+              <>
+                <div key={product.id} className="flex items-center gap-4">
+                  <div className="rounded-md border-2 shadow-2xl p-4">
+                    <Image
+                      width={500}
+                      height={500}
+                      className="w-14"
+                      src={product.image}
+                      alt="product"
+                    />
+                  </div>
+                  <div className="flex flex-col justify-between w-full gap-2">
+                    <h3 className="font-bold">{product.title}</h3>
+                    <span className="text-muted-foreground">
+                      ${product.price} USD
+                    </span>
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center gap-4 text-sm">
+                        <p>Quantity: {product.quantity}</p>
+                        <div className="flex items-center gap-2">
+                          <Button
+                            disabled={product.quantity === 1}
+                            onClick={() => sustractQuantity(product.id)}
+                            className="h-8"
+                          >
+                            -
+                          </Button>
+                          <Button
+                            onClick={() => addQuantity(product.id)}
+                            className="h-8"
+                          >
+                            +
+                          </Button>
+                        </div>
+                      </div>
+                      <DialogTrigger asChild>
+                        <Button className="h-8" size={"icon"}>
+                          <Trash className="w-4 h-4" />
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent>
+                        <div className="flex flex-col gap-12 p-4">
+                          <DialogHeader className="flex flex-col items-center gap-6">
+                            <HelpCircle className="w-20 h-20 mx-auto" />
+                            <h3 className="font-bold text-xl text-center uppercase">
+                              Are you sure you want to remove this product?
+                            </h3>
+                          </DialogHeader>
+                          <div className="flex justify-end gap-4">
+                            <DialogClose
+                              asChild
+                              onClick={() => removeProduct(product.id)}
                             >
-                              -
-                            </Button>
-                            <Button
-                              onClick={() => addQuantity(product.id)}
-                              className="h-8"
-                            >
-                              +
-                            </Button>
+                              <Button>Yes</Button>
+                            </DialogClose>
+                            <DialogClose asChild>
+                              <Button>No</Button>
+                            </DialogClose>
                           </div>
                         </div>
-                        <DialogTrigger asChild>
-                          <Button className="h-8" size={"icon"}>
-                            <Trash className="w-4 h-4" />
-                          </Button>
-                        </DialogTrigger>
-                        <DialogContent>
-                          <div className="flex flex-col gap-12 p-4">
-                            <DialogHeader className="flex flex-col items-center gap-6">
-                              <HelpCircle className="w-20 h-20 mx-auto" />
-                              <h3 className="font-bold text-xl text-center uppercase">
-                                Are you sure you want to remove this product?
-                              </h3>
-                            </DialogHeader>
-                            <div className="flex justify-end gap-4">
-                              <DialogClose
-                                asChild
-                                onClick={() => removeProduct(product.id)}
-                              >
-                                <Button>Yes</Button>
-                              </DialogClose>
-                              <DialogClose asChild>
-                                <Button>No</Button>
-                              </DialogClose>
-                            </div>
-                          </div>
-                        </DialogContent>
-                      </div>
+                      </DialogContent>
                     </div>
                   </div>
-                  <Separator className="h-0.5 bg-muted-foreground" />
-                </>
-              ))}
-            </div>
+                </div>
+                <Separator className="h-0.5 bg-muted-foreground" />
+              </>
+            ))}
+          </div>
           <Payment />
         </div>
-        <div className="flex flex-col gap-4 border-l pl-8">
+        <div
+          className={`flex flex-col gap-8 border-l pl-8 ${
+            cart.length === 0 && "cursor-not-allowed opacity-50"
+          }`}
+        >
           <h2 className="font-bold text-lg">Payment Details</h2>
           <div className="space-y-4">
             <div className="space-y-2">
               <label htmlFor="name" className="text-muted-foreground text-sm">
                 Name
               </label>
-              <Input placeholder="Freddy Tomada" />
+              <Input disabled={cart.length === 0} placeholder="Freddy Tomada" />
             </div>
             <div className="space-y-2">
               <label htmlFor="name" className="text-muted-foreground text-sm">
                 Credit Card Number
               </label>
               <PatternFormat
+                disabled={cart.length === 0}
                 customInput={Input}
                 format="#### #### #### ####"
                 placeholder="XXXX XXXX XXXX XXXX"
@@ -183,6 +193,7 @@ export default function Cart() {
                   Expiry Date
                 </label>
                 <PatternFormat
+                  disabled={cart.length === 0}
                   customInput={Input}
                   format="##/##"
                   placeholder="MM/YY"
@@ -193,6 +204,7 @@ export default function Cart() {
                   CVV
                 </label>
                 <PatternFormat
+                  disabled={cart.length === 0}
                   customInput={Input}
                   format="###"
                   placeholder="XXX"
@@ -206,7 +218,11 @@ export default function Cart() {
             <p className="font-bold text-xl">${total} USD</p>
           </div>
           <div className="w-full">
-            <Button onClick={makePayment} className="w-full h-12 mt-auto">
+            <Button
+              disabled={cart.length === 0}
+              onClick={makePayment}
+              className="w-full h-12 mt-auto"
+            >
               Make Payment
             </Button>
           </div>
