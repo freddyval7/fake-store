e5ce93f35209fa1c599bdccc18b711e430a100f9
Freddy Tomada <freddytomada7@gmail.com>
2024-03-01 10:13:53 -0400

    format cart

--- app/cart/cart.tsx
+++ app/cart/cart.tsx
@@ -31,7 +31,11 @@ export default function Cart() {
   }, []);
 
   useEffect(() => {
-    setTotal(+cart.reduce((acc, product) => acc + (product.price * product.quantity), 0).toFixed(2));
+    setTotal(
+      +cart
+        .reduce((acc, product) => acc + product.price * product.quantity, 0)
+        .toFixed(2)
+    );
   }, [cart]);
 
   function removeProduct(id: number) {
@@ -67,7 +71,8 @@ export default function Cart() {
   function makePayment() {
     toast({
       title: "Payment successful",
-      description: "Your payment has been processed successfully, you will be redirected to the home page",
+      description:
+        "Your payment has been processed successfully, you will be redirected to the home page",
     });
     setTimeout(() => {
       router.push("/");
@@ -81,76 +86,76 @@ export default function Cart() {
       <div className="grid md:grid-cols-2 gap-8">
         <div className="space-y-4">
           <h2 className="font-bold text-lg">Your shopping cart</h2>
-          <div className="space-y-4">
-            {cart.map((product) => (
-              <>
-                <div key={product.id} className="flex items-center gap-4">
-                  <div className="rounded-md border-2 shadow-2xl p-4">
-                    <Image
-                      width={500}
-                      height={500}
-                      className="w-14"
-                      src={product.image}
-                      alt="product"
-                    />
-                  </div>
-                  <div className="flex flex-col justify-between w-full gap-2">
-                    <h3 className="font-bold">{product.title}</h3>
-                    <span className="text-muted-foreground">
-                      ${product.price} USD
-                    </span>
-                    <div className="flex items-center justify-between">
-                      <div className="flex items-center gap-4 text-sm">
-                        <p>Quantity: {product.quantity}</p>
-                        <div className="flex items-center gap-2">
-                          <Button
-                            disabled={product.quantity === 1}
-                            onClick={() => sustractQuantity(product.id)}
-                            className="h-8"
-                          >
-                            -
-                          </Button>
-                          <Button
-                            onClick={() => addQuantity(product.id)}
-                            className="h-8"
-                          >
-                            +
-                          </Button>
-                        </div>
-                      </div>
-                      <DialogTrigger asChild>
-                        <Button className="h-8" size={"icon"}>
-                          <Trash className="w-4 h-4" />
-                        </Button>
-                      </DialogTrigger>
-                      <DialogContent>
-                        <div className="flex flex-col gap-12 p-4">
-                          <DialogHeader className="flex flex-col items-center gap-6">
-                            <HelpCircle className="w-20 h-20 mx-auto" />
-                            <h3 className="font-bold text-xl text-center uppercase">
-                              Are you sure you want to remove this product?
-                            </h3>
-                          </DialogHeader>
-                          <div className="flex justify-end gap-4">
-                            <DialogClose
-                              asChild
-                              onClick={() => removeProduct(product.id)}
+            <div className="space-y-4">
+              {cart.map((product) => (
+                <>
+                  <div key={product.id} className="flex items-center gap-4">
+                    <div className="rounded-md border-2 shadow-2xl p-4">
+                      <Image
+                        width={500}
+                        height={500}
+                        className="w-14"
+                        src={product.image}
+                        alt="product"
+                      />
+                    </div>
+                    <div className="flex flex-col justify-between w-full gap-2">
+                      <h3 className="font-bold">{product.title}</h3>
+                      <span className="text-muted-foreground">
+                        ${product.price} USD
+                      </span>
+                      <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-4 text-sm">
+                          <p>Quantity: {product.quantity}</p>
+                          <div className="flex items-center gap-2">
+                            <Button
+                              disabled={product.quantity === 1}
+                              onClick={() => sustractQuantity(product.id)}
+                              className="h-8"
                             >
-                              <Button>Yes</Button>
-                            </DialogClose>
-                            <DialogClose asChild>
-                              <Button>No</Button>
-                            </DialogClose>
+                              -
+                            </Button>
+                            <Button
+                              onClick={() => addQuantity(product.id)}
+                              className="h-8"
+                            >
+                              +
+                            </Button>
                           </div>
                         </div>
-                      </DialogContent>
+                        <DialogTrigger asChild>
+                          <Button className="h-8" size={"icon"}>
+                            <Trash className="w-4 h-4" />
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent>
+                          <div className="flex flex-col gap-12 p-4">
+                            <DialogHeader className="flex flex-col items-center gap-6">
+                              <HelpCircle className="w-20 h-20 mx-auto" />
+                              <h3 className="font-bold text-xl text-center uppercase">
+                                Are you sure you want to remove this product?
+                              </h3>
+                            </DialogHeader>
+                            <div className="flex justify-end gap-4">
+                              <DialogClose
+                                asChild
+                                onClick={() => removeProduct(product.id)}
+                              >
+                                <Button>Yes</Button>
+                              </DialogClose>
+                              <DialogClose asChild>
+                                <Button>No</Button>
+                              </DialogClose>
+                            </div>
+                          </div>
+                        </DialogContent>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <Separator className="h-0.5 bg-muted-foreground" />
-              </>
-            ))}
-          </div>
+                  <Separator className="h-0.5 bg-muted-foreground" />
+                </>
+              ))}
+            </div>
           <Payment />
         </div>
         <div className="flex flex-col gap-4 border-l pl-8">
@@ -201,7 +206,9 @@ export default function Cart() {
             <p className="font-bold text-xl">${total} USD</p>
           </div>
           <div className="w-full">
-            <Button onClick={makePayment} className="w-full h-12 mt-auto">Make Payment</Button>
+            <Button onClick={makePayment} className="w-full h-12 mt-auto">
+              Make Payment
+            </Button>
           </div>
         </div>
       </div>
