11754c1559e7b5624f9badc711dc2f701ed9f459
Freddy Tomada <freddytomada7@gmail.com>
2024-03-02 15:44:45 -0400

    add bg in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,13 @@
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, StoreIcon } from "lucide-react";
 import Link from "next/link";
 import SearchBox from "./search-box";
 import CategoriesButton from "./category-button";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center py-4 px-16 border-b shadow-md">
-      <Link href="/">
+    <div className="flex justify-between bg-primary text-primary-foreground items-center py-4 px-16 border-b shadow-md">
+      <Link className="flex items-center gap-4" href="/">
+        <StoreIcon className="h-5 w-5" />
         <span>U STORE</span>
       </Link>
       <div className="flex items-center gap-6">
