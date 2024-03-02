be962e0b5fd428916d07a73508b48a0014b99cea
Freddy Tomada <freddytomada7@gmail.com>
2024-03-02 15:38:43 -0400

    add filter in search page

--- app/search/filters.tsx
+++ app/search/filters.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Accordion,
   AccordionContent,
@@ -12,19 +14,38 @@ import {
   DialogHeader,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Settings2 } from "lucide-react";
+import { Settings2, Star } from "lucide-react";
+import { atom, useAtom } from "jotai";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export const FilterAtom = atom({
+  price: 1000,
+  rating: 5,
+});
 
 export default function Filters() {
+  const [filters, setFilters] = useAtom(FilterAtom);
+  const [price, setPrice] = useState(filters.price);
+  const [rating, setRating] = useState(filters.rating);
+
+  const router = useRouter();
+
   return (
     <>
       <div className="h-screen w-1/3 max-w-[20%] hidden md:block">
         <h2 className="text-lg">FILTERS</h2>
         <Separator />
-        <Accordion type="single" collapsible>
+        <Accordion type="multiple">
           <AccordionItem value="price">
             <AccordionTrigger>PRICE</AccordionTrigger>
             <AccordionContent className="pt-2 space-y-4">
-              <Slider />
+              <Slider
+                onValueChange={(value) => {
+                  setPrice(value[0] * 1000);
+                }}
+              />
               <div className="flex items-center justify-between">
                 <p>$0</p>
                 <p>$500</p>
@@ -32,14 +53,67 @@ export default function Filters() {
               </div>
             </AccordionContent>
           </AccordionItem>
+          <AccordionItem value="rating">
+            <AccordionTrigger>RATING</AccordionTrigger>
+            <AccordionContent className="pt-2 space-y-4">
+              <div className="flex items-center justify-between">
+                <Star
+                  className="cursor-pointer"
+                  fill={rating >= 1 ? "bg-primary" : "white"}
+                  onClick={() => {
+                    setRating(1);
+                  }}
+                />{" "}
+                <Star
+                  className="cursor-pointer"
+                  fill={rating >= 2 ? "bg-primary" : "white"}
+                  onClick={() => {
+                    setRating(2);
+                  }}
+                />{" "}
+                <Star
+                  className="cursor-pointer"
+                  fill={rating >= 3 ? "bg-primary" : "white"}
+                  onClick={() => {
+                    setRating(3);
+                  }}
+                />{" "}
+                <Star
+                  className="cursor-pointer"
+                  fill={rating >= 4 ? "bg-primary" : "white"}
+                  onClick={() => {
+                    setRating(4);
+                  }}
+                />{" "}
+                <Star
+                  className="cursor-pointer"
+                  fill={rating === 5 ? "bg-primary" : "white"}
+                  onClick={() => {
+                    setRating(5);
+                  }}
+                />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
         </Accordion>
+        <Button
+          onClick={() => {
+            setFilters({ ...filters, price: price, rating: rating });
+            router.refresh();
+          }}
+          className="w-full"
+        >
+          Apply
+        </Button>
       </div>
       <DialogTrigger className="md:hidden flex items-center gap-2 ml-auto border-2 p-2 rounded-full">
         <Settings2 />
         <span>Filters</span>
       </DialogTrigger>
       <DialogContent className="pt-10">
-        <DialogHeader className="uppercase font-bold text-lg">Filters</DialogHeader>
+        <DialogHeader className="uppercase font-bold text-lg">
+          Filters
+        </DialogHeader>
         <Accordion type="single" collapsible>
           <AccordionItem value="price">
             <AccordionTrigger>PRICE</AccordionTrigger>
--- app/search/search.tsx
+++ app/search/search.tsx
@@ -5,21 +5,36 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import Filters from "./filters";
+import Filters, { FilterAtom } from "./filters";
 import { Dialog } from "@/components/ui/dialog";
+import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
 
 export default function Search({ products }: { products: ProductType[] }) {
+  const [filter] = useAtom(FilterAtom)
+  const [productsFiltered, setProductsFiltered] = useState<ProductType[]>(products);
   const search = useSearchParams().get("searching") || "";
+
+  useEffect(() => {
+    console.log(filter);
+    const filtered = products.filter((product) => {
+      if (product.price <= filter.price && product.rating.rate <= filter.rating) {
+        return product;
+      }
+    });
+    setProductsFiltered(filtered);
+  }, [filter, products]);
+
   return (
     <Dialog>
       <div className="flex md:flex-row flex-col gap-8 md:gap-16">
         <Filters />
         <div className="space-y-8">
           <h2 className="text-2xl font-bold uppercase">
-            {products.length} results for the product {`"${search}"`}
+            {productsFiltered.length} results for the product {`"${search}"`}
           </h2>
           <div className="grid md:grid-cols-3">
-            {products.map((product) => (
+            {productsFiltered.map((product) => (
               <div
                 className="md:max-h-[400px] border-b-2 border-b-muted-foreground md:border-b-0 p-8 flex flex-col justify-between gap-4 md:gap-0 border-y-0 border-x shadow-none"
                 key={product.id}
